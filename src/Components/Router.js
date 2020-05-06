b9e21adfa0484a4837200ad78d839635c25a2371
favoritegithub <favoritemail.sooyoung@gmail.com>
2020-05-06 13:56:27 +0900

    Create Router & Switch

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import Home from 'Routes/Home';
 import TV from 'Routes/TV';
 import Search from 'Routes/Search';
@@ -7,10 +7,11 @@ import Search from 'Routes/Search';
 
 export default () => (
     <Router>
-        <>
+        <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/tv" exact component={TV} />
-            <Route path="/search" exact component={Search} />
-        </>
+            <Route path="/search" component={Search} />
+            <Redirect from="*" to="/" />
+        </Switch>
     </Router>
 )
